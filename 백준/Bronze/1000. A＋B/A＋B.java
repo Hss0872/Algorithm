d1ebdf2f45ed8d562ce5526d049f4a7fe0a99a0c
Kafo <83797266+Hss0872@users.noreply.github.com>
2025-01-16 10:34:44 +0900

    [Bronze V] Title: A+B, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNum1 = scanner.nextInt();
+        int inputNum2 = scanner.nextInt();
+
+        System.out.println(inputNum1 + inputNum2);
+    }
+}
